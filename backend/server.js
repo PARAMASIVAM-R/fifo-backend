591e2a27c424c66335b4cfa1d7835d4033990fd8
PARAMASIVAM-R <paramasivam3448@gmail.com>
2025-04-24 16:24:33 +0530

    mongodb url

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,14 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-dotenv.config(); // load .env variables
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("✅ MongoDB Connected!"))
     .catch(err => console.log("❌ MongoDB Connection Error:", err));
 
@@ -22,4 +19,5 @@ app.use("/api/godown", require("./src/routes/godownRoutes"));
 app.use("/api/pds", require("./src/routes/pdsRoutes"));
 app.use("/api/admin", require("./src/routes/adminRoutes"));
 
-app.listen(5000, () => console.log("✅ Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
